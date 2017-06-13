c8f7f31c71aa954f306afd897c804eee3e01ee6f
ahtesham quraish <ahtesham.quraish@arbisoft.com>
2017-06-13 18:36:49 +0500

    doing

--- app/app.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-angular
-  .module('app', [ 'ngRoute', 'pubnub.angular.service', 'ngNotify'])
--- app/services/current_user.service.js
+++
@@ -1,3 +0,0 @@
-angular.module('app')
-.value('currentUser', 'augsuten');
-//_.random(1000000).toString()
